73ba5c0f795cb442ed1236b2c1187d016f0aa6ad
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-17 17:11:43 +0200

    add filter option for selectAll() in RateManager

--- src/Model/RateManager.php
+++ src/Model/RateManager.php
@@ -6,9 +6,12 @@ class RateManager extends AbstractManager
 {
     public const TABLE = 'rate';
 
-    public function selectAll(string $orderBy = '', string $direction = 'ASC'): array
+    public function selectAll(string $filter = '', string $orderBy = '', string $direction = 'ASC'): array
     {
         $query = 'SELECT * FROM ' . static::TABLE . ' r INNER JOIN rate_category rc ON r.rate_category_id = rc.id';
+        if ($filter) {
+            $query .= ' WHERE ' . $filter;
+        }
         if ($orderBy) {
             $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
         }
