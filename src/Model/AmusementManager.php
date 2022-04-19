15cddc9901c717209fc82d13f01ebbdd38343fb5
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-19 09:29:48 +0200

    change self to static when calling TABLE in AmusementManager

--- src/Model/AmusementManager.php
+++ src/Model/AmusementManager.php
@@ -8,11 +8,11 @@ class AmusementManager extends AbstractManager
 
     public function selectFourRandom(): array
     {
-        $query = 'SELECT count(*) FROM ' . self::TABLE;
+        $query = 'SELECT count(*) FROM ' . static::TABLE;
         $totalRows = $this->pdo->query($query)->fetchColumn();
         if ($totalRows >= 5) {
             $randLimit = 'LIMIT ' . rand(0, $totalRows - 5) . ', 4';
-            $query = 'SELECT * FROM ' . self::TABLE . ' ' . $randLimit;
+            $query = 'SELECT * FROM ' . static::TABLE . ' ' . $randLimit;
             return $this->pdo->query($query)->fetchAll();
         }
         return [];
