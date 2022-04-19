21cbf8bf59120983c3772c7faf6ff58c97e90066
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-19 12:13:53 +0200

    removed selectFourRandom method from AmusementManager

--- src/Model/AmusementManager.php
+++ src/Model/AmusementManager.php
@@ -5,16 +5,4 @@ namespace App\Model;
 class AmusementManager extends AbstractManager
 {
     public const TABLE = 'amusement';
-
-    public function selectFourRandom(): array
-    {
-        $query = 'SELECT count(*) FROM ' . static::TABLE;
-        $totalRows = $this->pdo->query($query)->fetchColumn();
-        if ($totalRows >= 5) {
-            $randLimit = 'LIMIT ' . rand(0, $totalRows - 5) . ', 4';
-            $query = 'SELECT * FROM ' . static::TABLE . ' ' . $randLimit;
-            return $this->pdo->query($query)->fetchAll();
-        }
-        return [];
-    }
 }
