9cab350bed3ed01a3b6250f4bec22c816403a39a
codelande <corentin-delande_student2022@wilder.school>
2022-04-21 15:32:05 +0200

    change after alexis review

--- src/routes.php
+++ src/routes.php
@@ -7,8 +7,6 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 
-use App\Controller\CafeteriaController;
-
 return [
     '' => ['HomeController', 'index',],
     'items' => ['ItemController', 'index',],
