2327470ba136b517dc2143640702d2a374b3a7b7
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-04-21 10:08:38 +0200

    changed comment

--- src/Controller/AmusementsController.php
+++ src/Controller/AmusementsController.php
@@ -4,9 +4,7 @@ namespace App\Controller;
 
 class AmusementsController extends AbstractController
 {
-    /**
-     * Display home page
-     */
+
     public function index(): string
     {
         return $this->twig->render('Amusements/index.html.twig');
