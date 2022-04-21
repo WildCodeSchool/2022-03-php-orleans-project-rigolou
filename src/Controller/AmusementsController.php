55756be41cc9479607f161f19da3d50c639aaa38
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-04-21 15:02:11 +0200

    correction

--- src/Controller/AmusementsController.php
+++ src/Controller/AmusementsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-class AmusementsController extends AbstractController
+class AmusementController extends AbstractController
 {
     public function index(): string
     {
