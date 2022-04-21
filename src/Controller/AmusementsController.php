46aa71496821c778ad483b2b9a07a5de174608cb
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-04-21 16:16:37 +0200

    correction

--- src/Controller/AmusementsController.php
+++ src/Controller/AmusementsController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Model\AmusementManager;
 
-class AmusementsController extends AbstractController
+class AmusementController extends AbstractController
 {
     public function index(): string
     {
