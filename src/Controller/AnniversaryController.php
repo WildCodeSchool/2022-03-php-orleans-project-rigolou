1f022cca26f06b36f90608001ff774ba9d9bb286
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-27 11:59:43 +0200

    route, controller, manager, view

---
+++ src/Controller/AnniversaryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+class AnniversaryController extends AbstractController
+{
+    /**
+     * Display home page
+     */
+    public function index(): string
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+}
