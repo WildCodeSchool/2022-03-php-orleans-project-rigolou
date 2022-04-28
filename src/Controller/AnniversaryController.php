ca03126c190a63e6e32c81f8d361f15829b07e33
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-04-28 16:12:04 +0200

    errors create

--- src/Controller/AnniversaryController.php
+++ src/Controller/AnniversaryController.php
@@ -8,37 +8,44 @@ class AnniversaryController extends AbstractController
 {
     public function index()
     {
-        $errors = [];
+        $errors = null;
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $reservation = array_map('trim', $_POST);
-
-            if (empty($reservation['firstname'])) {
-                $errors[] = 'Le prénom est obligatoire';
-            }
-
-            if (empty($reservation['lastname'])) {
-                $errors[] = 'Le nom est obligatoire';
-            }
-
-            if (empty($reservation['email'])) {
-                $errors[] = 'L\'e-mail est obligatoire';
-            } elseif (!filter_var($reservation['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors[] = 'L\'e-mail n\'a pas le bon format';
-            }
-
-
-            if (empty($reservation['message'])) {
-                $errors[] = 'Le message est obligatoire';
-            }
-
-            if (empty($reservation['date'])) {
-                $errors[] = 'La date est obligatoire';
-            }
-
-            if (empty($reservation['phone'])) {
-                $errors[] = 'Le numéro de téléphone est obligatoire';
-            }
+            $errors = $this->validate($reservation);
         }
         return $this->twig->render('Anniversary/index.html.twig', ['errors' => $errors]);
+    }
+
+    public function validate(array $reservation): array
+    {
+
+        $errors = [];
+        if (empty($reservation['firstname'])) {
+            $errors[] = 'Le prénom est obligatoire';
+        }
+
+        if (empty($reservation['lastname'])) {
+            $errors[] = 'Le nom est obligatoire';
+        }
+
+        if (empty($reservation['email'])) {
+            $errors[] = 'L\'e-mail est obligatoire';
+        } elseif (!filter_var($reservation['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'L\'e-mail n\'a pas le bon format';
+        }
+
+        if (empty($reservation['message'])) {
+            $errors[] = 'Le message est obligatoire';
+        }
+
+        if (empty($reservation['date'])) {
+            $errors[] = 'La date est obligatoire';
+        }
+
+        if (empty($reservation['phone'])) {
+            $errors[] = 'Le numéro de téléphone est obligatoire';
+        }
+        return $errors;
     }
 }
