00bcfc8245121c9b7cbb5de3d650544f8320b567
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-04-28 10:35:55 +0200

    method post verification in method index() and simplified how we treat inputs and errors

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -18,50 +18,50 @@ class ContactController extends AbstractController
             $messageSend = true;
         }
 
-        $validations = $this->validateContactPost();
-
-        if (isset($validations['errors']) && empty($validations['errors'])) {
-            $this->sendMail($validations['inputs']);
-            header('Location: /contact?send=success');
+        $errors = [];
+        $inputs = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $inputs = array_map('trim', $_POST);
+            $errors = $this->validateContactPost($inputs);
+            if (empty($errors)) {
+                $this->sendMail($inputs);
+                header('Location: /contact?send=success');
+            }
         }
 
         return $this->twig->render('Contact/index.html.twig', [
-            'validations' => $validations,
-            'messageSend' => $messageSend
+            'errors' => $errors,
+            'inputs' => $inputs,
+            'messageSend' => $messageSend,
         ]);
     }
 
 
-    private function validateContactPost(): array
+    private function validateContactPost(array $inputs): array
     {
-        $validations = [];
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $validations['errors'] = [];
-            $validations['inputs'] = array_map('trim', $_POST);
-
-            if (empty($validations['inputs']['firstname'])) {
-                $validations['errors']['firstname'] = 'Le prénom est obligatoire';
-            }
-
-            if (empty($validations['inputs']['lastname'])) {
-                $validations['errors']['lastname'] = 'Le nom est obligatoire';
-            }
-
-            if (empty($validations['inputs']['email'])) {
-                $validations['errors']['email'] = 'L\'e-mail est obligatoire';
-            } elseif (!filter_var($validations['inputs']['email'], FILTER_VALIDATE_EMAIL)) {
-                $validations['errors']['email'] = 'L\'e-mail n\'a pas le bon format';
-            }
-
-            if (empty($validations['inputs']['subject'])) {
-                $validations['errors']['subject'] = 'L\'objet est obligatoire';
-            }
-
-            if (empty($validations['inputs']['message'])) {
-                $validations['errors']['message'] = 'Le message est obligatoire';
-            }
+        $errors = [];
+        if (empty($inputs['firstname'])) {
+            $errors['firstname'] = 'Le prénom est obligatoire';
         }
-        return $validations;
+
+        if (empty($inputs['lastname'])) {
+            $errors['lastname'] = 'Le nom est obligatoire';
+        }
+
+        if (empty($inputs['email'])) {
+            $errors['email'] = 'L\'e-mail est obligatoire';
+        } elseif (!filter_var($inputs['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'L\'e-mail n\'a pas le bon format';
+        }
+
+        if (empty($inputs['subject'])) {
+            $errors['subject'] = 'L\'objet est obligatoire';
+        }
+
+        if (empty($inputs['message'])) {
+            $errors['message'] = 'Le message est obligatoire';
+        }
+        return $errors;
     }
 
     private function sendMail(array $inputs): void
