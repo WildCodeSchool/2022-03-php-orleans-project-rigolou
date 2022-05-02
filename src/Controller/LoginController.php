fec3e619a675b2126c8553edab89c8426d095bf1
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-05-02 22:31:18 +0200

    validations and password verification

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,7 +8,37 @@ class LoginController extends AbstractController
 {
     public function login(): string
     {
-        return $this->twig->render('Login/login.html.twig');
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $emailAndPassword = array_map('trim', $_POST);
+            if (empty($emailAndPassword['email'])) {
+                $errors[] = 'L\'email est obligatoire';
+            }
+            if (empty($emailAndPassword['password'])) {
+                $errors[] = 'Le mot de passe est obligatoire';
+            }
+            if (!filter_var($emailAndPassword['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors[] = 'Le format d\'email est invalide';
+            }
+
+            if (empty($errors)) {
+                $userManager = new UserManager();
+                $user = $userManager->selectOneByEmail($emailAndPassword['email']);
+                if ($user) {
+                    if (password_verify($emailAndPassword['password'], $user['password'])) {
+                        $_SESSION['user'] = $user['id'];
+                        header('Location: /');
+                    } else {
+                        $errors[] = 'Mauvais mot de passe';
+                    }
+                } else {
+                    $errors[] = 'Email inconnu';
+                }
+            }
+        }
+        return $this->twig->render('Login/login.html.twig', [
+            'errors' => $errors,
+        ]);
     }
 
     public function logout()
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -5,4 +5,13 @@ namespace App\Model;
 class UserManager extends AbstractManager
 {
     public const TABLE = 'user';
+
+    public function selectOneByEmail(string $email): array|false
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE email=:email");
+        $statement->bindValue('email', $email, \PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
 }
