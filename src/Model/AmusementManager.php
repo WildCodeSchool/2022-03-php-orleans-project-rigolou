04c3f7d97f4781fbd47f6687a6279353ee0afc48
Gliem Maximilian <maximilian.gliem@gmail.com>
2022-05-03 13:41:10 +0200

    manager modifications

--- src/Model/AmusementManager.php
+++ src/Model/AmusementManager.php
@@ -17,4 +17,34 @@ class AmusementManager extends AbstractManager
 
          $statement->execute();
     }
+
+    public function updateWithImage(array $items): void
+    {
+        $query = 'UPDATE ' . self::TABLE . ' set 
+        name=:name, 
+        description=:description, 
+        image=:image 
+        WHERE id=:id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $items['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $items['description'], \PDO::PARAM_STR);
+        $statement->bindValue('image', $items['image'], \PDO::PARAM_STR);
+        $statement->bindValue('id', $items['id'], \PDO::PARAM_INT);
+
+        $statement->execute();
+    }
+
+    public function updateWithoutImage(array $items): void
+    {
+        $query = 'UPDATE ' . self::TABLE . ' set 
+        name=:name, 
+        description=:description 
+        WHERE id=:id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('name', $items['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $items['description'], \PDO::PARAM_STR);
+        $statement->bindValue('id', $items['id'], \PDO::PARAM_INT);
+
+        $statement->execute();
+    }
 }
