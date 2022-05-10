47e5830ff84ca730ae203fa9e1fb4c9847f1d737
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-05-10 09:19:01 +0200

    fix delete for reservation admin

--- src/Controller/AdminAnniversaryController.php
+++ src/Controller/AdminAnniversaryController.php
@@ -15,7 +15,6 @@ class AdminAnniversaryController extends AbstractController
     }
     public function delete()
     {
-
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $id = trim($_POST['id']);
             $reservationManager = new AnniversaryManager();
